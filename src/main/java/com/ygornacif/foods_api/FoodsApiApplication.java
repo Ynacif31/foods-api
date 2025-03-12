36633c4919df705e44b0a8bc93c8509d173ef219
Ygor Nacif <ygor.nacif00@gmail.com>
2025-03-12 16:50:26 -0300

    fix: project started and aws config started

---
+++ src/main/java/com/ygornacif/foods_api/FoodsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ygornacif.foods_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ygornacif/foods_api/FoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygornacif.foods_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
