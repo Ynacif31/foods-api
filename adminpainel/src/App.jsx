58174ef21b6f5ebbf2b6ccb2c52535eb85f717af
Ygor Nacif <ygor.nacif00@gmail.com>
2025-03-17 16:08:00 -0300

    feat: all styles from painel is add

--- adminpainel/src/App.jsx
+++ adminpainel/src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react'; // Corrigir a importação do useState
 import { Route, Routes } from 'react-router-dom';
 import ListFood from './pages/ListFood/ListFood';
 import Sidebar from './components/Sidebar/Sidebar';
 import AddFood from './pages/AddFood/AddFood';
 import Menubar from './components/Menubar/Menubar';
-import Orders from './pages/Orders/Orders'; 
+import Orders from './pages/Orders/Orders';
 
 const App = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
   return (
     <div className="d-flex" id="wrapper">
-      <Sidebar />
+      <Sidebar sidebarVisible={sidebarVisible} />
       <div id="page-content-wrapper">
-        <Menubar />
+        <Menubar toggleSidebar={toggleSidebar} />
         <div className="container-fluid">
           <Routes>
-            <Route path='/add' element={<AddFood />} />
+            <Route path='/add-food' element={<AddFood />} />
             <Route path='/list-food' element={<ListFood />} />
             <Route path='/orders' element={<Orders />} />
             <Route path='/' element={<ListFood />} />
@@ -26,3 +32,4 @@ const App = () => {
 };
 
 export default App;
+
--- adminpainel/src/components/Sidebar/Sidebar.jsx
+++ adminpainel/src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { assets } from '../../assets/assets';
+import {} from '../../assets/assets';
 
-const Sidebar = () => {
-return (
-    <div className="border-end bg-white" id="sidebar-wrapper">
-    <div className="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
-    <div className="list-group list-group-flush">
-    <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add">AddFood</Link>
-    <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/list-food">ListFood</Link>
-    <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/orders">Orders</Link>
+const Sidebar = ({ sidebarVisible }) => {
+  return (
+    <div className={`border-end bg-white ${sidebarVisible ? '' : 'd-none'}`} id="sidebar-wrapper">
+      <div className="sidebar-heading border-bottom bg-light">
+        <img src={assets.logo} alt="" height={32} width={32} className='rounded-circle'/>
+        </div>
+      <div className="list-group list-group-flush">
+        <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add-food">
+        <i className='bi bi-plus-circle me-2'></i> AddFood</Link>
+        <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/list-food">
+        <i className='bi bi-list-ul me-2'></i>ListFood</Link>
+        <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/orders">
+        <i className='bi bi-cart me-2'></i>Orders</Link>
+      </div>
     </div>
-</div>
-)
-}
+  );
+};
 
 export default Sidebar;
